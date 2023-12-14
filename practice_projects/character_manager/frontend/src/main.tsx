8d2dfff05faa7b3fbaf2d3d8e65effb2cd72bf78
Brett Pittman <agbrettpittman@gmail.com>
2023-12-13 20:53:42 -0500

    Got started on frontend

---
+++ practice_projects/character_manager/frontend/src/main.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "@routes/root";
+import "./index.css";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+const RootElement = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(RootElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

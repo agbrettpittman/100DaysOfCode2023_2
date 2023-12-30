00e17d44d8a77a47cef90cd7a340e8fd6ee0bcc5
Brett Pittman <agbrettpittman@gmail.com>
2023-12-29 21:04:30 -0500

    improved landing page

--- practice_projects/character_manager/frontend/src/routes/Index.tsx
+++ practice_projects/character_manager/frontend/src/routes/Index.tsx
@@ -1,13 +1,15 @@
+import { Typography, Container } from "@mui/material";
+import styled from "styled-components";
+
+const StyledContainer = styled(Container)`
+    opacity: 0.5;
+`
+
 export default function Index() {
     return (
-        <p id="zero-state">
-            This is a demo for React Router.
-            <br />
-            Check out{" "}
-            <a href="https://reactrouter.com">
-            the docs at reactrouter.com
-            </a>
-            .
-        </p>
+        <StyledContainer>
+            <Typography component="h1" variant="h3" align='center' sx={{ fontWeight: "lighter", mt:16 }}>Lair of the Ancients</Typography>
+            <Typography variant="subtitle1" align='center'>Welcome! You can find your characters on the sidebar and other's in the search bar</Typography>
+        </StyledContainer>
     );
 }

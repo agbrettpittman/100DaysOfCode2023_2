3afa6892917b4de982fdc04a047d778de656cc86
Brett Pittman <agbrettpittman@gmail.com>
2024-02-02 21:50:11 -0500

    Add bcrypt dependency and implement user signup functionality

--- courses/net_ninja/MERN-Auth-Tutorial/backend/controllers/userController.js
+++ courses/net_ninja/MERN-Auth-Tutorial/backend/controllers/userController.js
@@ -1,13 +1,19 @@
 const User = require("../models/userModel")
 
 async function loginUser(req, res) {
-    const { email, password, id } = req.body
+    const { email, password } = req.body
     res.json({ mssg: "Login user" })
 }
 
 async function signupUser(req, res) {
     const { email, password } = req.body
-    res.json({ mssg: "Signup user" })
+
+    try {
+        const user = await User.signup(email, password)
+        res.status(200).json({ user })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 module.exports = {

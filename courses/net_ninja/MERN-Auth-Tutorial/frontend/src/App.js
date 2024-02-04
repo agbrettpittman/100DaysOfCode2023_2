887cef6bfe5f98c972fd73a9ab38814332b83946
Brett Pittman <agbrettpittman@gmail.com>
2024-02-04 16:59:33 -0500

    Update routes and add login and signup pages to App.js and Navbar.js

--- courses/net_ninja/MERN-Auth-Tutorial/frontend/src/App.js
+++ courses/net_ninja/MERN-Auth-Tutorial/frontend/src/App.js
@@ -1,25 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 // pages & components
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="pages">
-          <Routes>
-            <Route 
-              path="/"
-              element={<Home />}
-            />
-          </Routes>
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <div className="pages">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
-      </BrowserRouter>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App

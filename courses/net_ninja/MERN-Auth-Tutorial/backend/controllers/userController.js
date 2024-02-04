2c27a91e9675bb7f4d94c795fe8319b0968b9d15
Brett Pittman <agbrettpittman@gmail.com>
2024-02-04 16:29:03 -0500

    Add JWT token authentication to user signup

--- courses/net_ninja/MERN-Auth-Tutorial/backend/controllers/userController.js
+++ courses/net_ninja/MERN-Auth-Tutorial/backend/controllers/userController.js
@@ -1,4 +1,11 @@
 const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
+
+function createToken(_id) {
+    return jwt.sign({ _id }, process.env.SECRET, {
+        expiresIn: "3d",
+    })
+}
 
 async function loginUser(req, res) {
     const { email, password } = req.body
@@ -10,7 +17,11 @@ async function signupUser(req, res) {
 
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({ user })
+
+        //create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({ email, token })
     } catch (error) {
         res.status(400).json({ error: error.message })
     }

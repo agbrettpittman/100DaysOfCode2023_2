f1a54172a31be4e2e5024391c020098092d54a1b
Brett Pittman <agbrettpittman@gmail.com>
2024-02-08 18:12:51 -0500

    Update routes to use JWT authentication middleware

--- courses/david_gray/MERN_Stack_Auth/backend/routes/noteRoutes.js
+++ courses/david_gray/MERN_Stack_Auth/backend/routes/noteRoutes.js
@@ -1,8 +1,12 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const notesController = require('../controllers/notesController')
+const notesController = require("../controllers/notesController")
+const verifyJWT = require("../middleware/verifyJWT")
 
-router.route('/')
+router.use(verifyJWT)
+
+router
+    .route("/")
     .get(notesController.getAllNotes)
     .post(notesController.createNewNote)
     .patch(notesController.updateNote)
--- courses/david_gray/MERN_Stack_Auth/backend/routes/userRoutes.js
+++ courses/david_gray/MERN_Stack_Auth/backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const usersController = require('../controllers/usersController')
+const usersController = require("../controllers/usersController")
+const verifyJWT = require("../middleware/verifyJWT")
 
-router.route('/')
+router.use(verifyJWT)
+
+router
+    .route("/")
     .get(usersController.getAllUsers)
     .post(usersController.createNewUser)
     .patch(usersController.updateUser)

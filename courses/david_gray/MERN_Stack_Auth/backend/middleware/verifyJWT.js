070cec4f39f27891cea235488a1f2f270e065e91
Brett Pittman <agbrettpittman@gmail.com>
2024-02-08 18:12:25 -0500

    Add token extraction in verifyJWT middleware

--- courses/david_gray/MERN_Stack_Auth/backend/middleware/verifyJWT.js
+++ courses/david_gray/MERN_Stack_Auth/backend/middleware/verifyJWT.js
@@ -7,6 +7,8 @@ function verifyJWT(req, res, next) {
         return res.status(401).json({ message: "Unauthorized" })
     }
 
+    const token = authHeader.split(" ")[1]
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
             return res.status(403).json({ message: "Forbidden" })

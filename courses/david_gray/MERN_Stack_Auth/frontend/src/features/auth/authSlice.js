13fe29d9b7246251cb68e249c3af10312ff91832
Brett Pittman <agbrettpittman@gmail.com>
2024-02-11 14:58:20 -0500

    fixed reducer placement

--- courses/david_gray/MERN_Stack_Auth/frontend/src/features/auth/authSlice.js
+++ courses/david_gray/MERN_Stack_Auth/frontend/src/features/auth/authSlice.js
@@ -4,14 +4,14 @@ const authSlice = createSlice({
     name: "auth",
     initialState: {
         token: null,
-        reducers: {
-            setCredentials: (state, action) => {
-                const { accessToken } = action.payload
-                state.token = accessToken
-            },
-            logOut: (state, action) => {
-                state.token = null
-            },
+    },
+    reducers: {
+        setCredentials: (state, action) => {
+            const { accessToken } = action.payload
+            state.token = accessToken
+        },
+        logOut: (state, action) => {
+            state.token = null
         },
     },
 })

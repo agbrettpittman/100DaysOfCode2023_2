c5c821cbc5aa563c2f48daf2e149337f571196b5
Brett Pittman <agbrettpittman@gmail.com>
2024-02-18 20:23:36 -0500

    blocking attempts to manually access note editing

--- courses/david_gray/MERN_Stack_Auth/frontend/src/features/notes/EditNote.js
+++ courses/david_gray/MERN_Stack_Auth/frontend/src/features/notes/EditNote.js
@@ -1,9 +1,13 @@
+import { useParams } from "react-router-dom"
+import EditNoteForm from "./EditNoteForm"
 import { useGetNotesQuery } from "./notesApiSlice"
 import { useGetUsersQuery } from "../users/usersApiSlice"
 import useAuth from "../../hooks/useAuth"
 
 const EditNote = () => {
     const { id } = useParams()
+
+    const { username, isManager, isAdmin } = useAuth()
 
     const { note } = useGetNotesQuery("notesList", {
         selectFromResult: ({ data }) => ({
@@ -16,5 +20,8 @@ const EditNote = () => {
             users: data?.ids.map((id) => data?.entities[id]),
         }),
     })
+    if (!isManager && !isAdmin && note.username !== username) return <p className="errmsg">No Access</p>
+
+    return <EditNoteForm note={note} users={users} />
 }
 export default EditNote

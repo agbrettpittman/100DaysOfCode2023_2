c1ec059f5cdb36fd9e2ca23bb74d6ea4a7d15c61
Brett Pittman <agbrettpittman@gmail.com>
2024-02-18 20:21:20 -0500

    optimized RTK prefetch queries

--- courses/david_gray/MERN_Stack_Auth/frontend/src/features/notes/EditNote.js
+++ courses/david_gray/MERN_Stack_Auth/frontend/src/features/notes/EditNote.js
@@ -1,17 +1,20 @@
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectNoteById } from './notesApiSlice'
-import { selectAllUsers } from '../users/usersApiSlice'
-import EditNoteForm from './EditNoteForm'
+import { useGetNotesQuery } from "./notesApiSlice"
+import { useGetUsersQuery } from "../users/usersApiSlice"
+import useAuth from "../../hooks/useAuth"
 
 const EditNote = () => {
     const { id } = useParams()
 
-    const note = useSelector(state => selectNoteById(state, id))
-    const users = useSelector(selectAllUsers)
+    const { note } = useGetNotesQuery("notesList", {
+        selectFromResult: ({ data }) => ({
+            note: data?.entities[id],
+        }),
+    })
 
-    const content = note && users ? <EditNoteForm note={note} users={users} /> : <p>Loading...</p>
-
-    return content
+    const { users } = useGetUsersQuery("usersList", {
+        selectFromResult: ({ data }) => ({
+            users: data?.ids.map((id) => data?.entities[id]),
+        }),
+    })
 }
 export default EditNote
--- courses/david_gray/MERN_Stack_Auth/frontend/src/features/notes/Note.js
+++ courses/david_gray/MERN_Stack_Auth/frontend/src/features/notes/Note.js
@@ -1,20 +1,27 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
-import { useNavigate } from 'react-router-dom'
-
-import { useSelector } from 'react-redux'
-import { selectNoteById } from './notesApiSlice'
+import { useGetNotesQuery } from "./notesApiSlice"
+import { memo } from "react"
 
 const Note = ({ noteId }) => {
-
-    const note = useSelector(state => selectNoteById(state, noteId))
+    const { note } = useGetNotesQuery("notesList", {
+        selectFromResult: ({ data }) => ({
+            note: data?.entities[noteId],
+        }),
+    })
 
     const navigate = useNavigate()
 
     if (note) {
-        const created = new Date(note.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long' })
+        const created = new Date(note.createdAt).toLocaleString("en-US", {
+            day: "numeric",
+            month: "long",
+        })
 
-        const updated = new Date(note.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long' })
+        const updated = new Date(note.updatedAt).toLocaleString("en-US", {
+            day: "numeric",
+            month: "long",
+        })
 
         const handleEdit = () => navigate(`/dash/notes/${noteId}`)
 

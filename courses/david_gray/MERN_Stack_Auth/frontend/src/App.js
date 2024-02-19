1c7754a8293508f1217f8d6411b7fc19b78e56ab
Brett Pittman <agbrettpittman@gmail.com>
2024-02-18 20:41:20 -0500

    Add useTitle hook for dynamically updating document title

--- courses/david_gray/MERN_Stack_Auth/frontend/src/App.js
+++ courses/david_gray/MERN_Stack_Auth/frontend/src/App.js
@@ -14,44 +14,27 @@ import Prefetch from "./features/auth/Prefetch"
 import PersistLogin from "./features/auth/PersistLogin"
 import { ROLES } from "./config/roles"
 import RequireAuth from "./features/auth/RequireAuth"
+import useTitle from "./hooks/useTitle"
 
 function App() {
+    useTitle("Dan D. Repairs")
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Public />} />
                 <Route path="login" element={<Login />} />
 
-                <Route
-                    element={
-                        <RequireAuth allowedRoles={[...Object.values(ROLES)]} />
-                    }
-                >
+                <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}>
                     <Route element={<PersistLogin />}>
                         <Route element={<Prefetch />}>
                             <Route path="dash" element={<DashLayout />}>
                                 <Route index element={<Welcome />} />
 
-                                <Route
-                                    element={
-                                        <RequireAuth
-                                            allowedRoles={[
-                                                ROLES.Manager,
-                                                ROLES.Admin,
-                                            ]}
-                                        />
-                                    }
-                                >
+                                <Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />}>
                                     <Route path="users">
                                         <Route index element={<UsersList />} />
-                                        <Route
-                                            path=":id"
-                                            element={<EditUser />}
-                                        />
-                                        <Route
-                                            path="new"
-                                            element={<NewUserForm />}
-                                        />
+                                        <Route path=":id" element={<EditUser />} />
+                                        <Route path="new" element={<NewUserForm />} />
                                     </Route>
                                 </Route>
                                 <Route path="notes">

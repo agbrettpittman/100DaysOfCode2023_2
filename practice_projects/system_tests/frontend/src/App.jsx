989741ad931b2fc419420848d0908a54828e1da2
Brett Pittman <agbrettpittman@gmail.com>
2024-02-23 20:50:47 -0500

    initial react setup

---
+++ practice_projects/system_tests/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <main className='container'>
+        <h1>System Test Tracker</h1>
+    </main>
+  )
+}
+
+export default App

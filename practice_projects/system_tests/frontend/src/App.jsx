3de3838c8f1c0d2d775eba72f68baf6e7ceb22f9
Brett Pittman <agbrettpittman@gmail.com>
2024-02-23 21:05:19 -0500

    Basic Layout

--- practice_projects/system_tests/frontend/src/App.jsx
+++ practice_projects/system_tests/frontend/src/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 
 function App() {
 
   return (
-    <main className='container'>
-        <h1>System Test Tracker</h1>
-    </main>
+    <BrowserRouter>
+        <main className='container'>
+            <nav>
+                <ul>
+                    <li><Link to="/" className="contrast"><strong>System Test Tracker</strong></Link></li>
+                </ul>
+                <ul>
+                    <li><Link to="/systems">Systems</Link></li>
+                    <li><Link to="/tests">Tests</Link></li>
+                </ul>
+            </nav>
+            <Routes>
+                {/*<Route path="/" element={<Home />} />*/}
+                {/*<Route path="/404" element={<NotFound />} />*/}
+                {/*<Route path="/tests" element={<TestList />} />*/}
+                {/*<Route path="/systems" element={<SystemList />} />*/}
+                {/*<Route path="*" element={<Navigate replace to="/404" />} />*/}
+            </Routes>
+        </main>
+    </BrowserRouter>
   )
 }
 
